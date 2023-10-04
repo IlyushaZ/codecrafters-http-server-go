cc866349371d29032e0c75d1ebb74812288fd7b0
Ilya Zyabirov <izyabirov@ptsecurity.com>
2023-10-04 15:22:06 +0300

    stage #2

--- app/server.go
+++ app/server.go
@@ -6,6 +6,19 @@ import (
 	"os"
 )
 
+func respond(conn net.Conn, status int, stringStatus string) error {
+	buf := make([]byte, 1024)
+	if _, err := conn.Read(buf); err != nil {
+		return fmt.Errorf("can't read from conn: %w", err)
+	}
+
+	if _, err := fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n\r\n", status, stringStatus); err != nil {
+		return fmt.Errorf("can't write to conn: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -16,9 +29,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	if err := respond(conn, 200, "OK"); err != nil {
+		fmt.Println("Error responding: ", err.Error())
 		os.Exit(1)
 	}
 }
