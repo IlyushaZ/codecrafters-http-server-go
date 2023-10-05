87f4df5d5d181fabd13e0e698a5b05aa5c11bb85
Ilya Zyabirov <izyabirov@ptsecurity.com>
2023-10-05 15:35:09 +0300

    stage #3

--- app/server.go
+++ app/server.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func respond(conn net.Conn, status int, stringStatus string) error {
-	buf := make([]byte, 1024)
-	if _, err := conn.Read(buf); err != nil {
-		return fmt.Errorf("can't read from conn: %w", err)
-	}
-
 	if _, err := fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n\r\n", status, stringStatus); err != nil {
 		return fmt.Errorf("can't write to conn: %w", err)
 	}
 
 	return nil
+}
+
+func checkStartLine(conn net.Conn) error {
+	buf := make([]byte, 1024)
+	if _, err := conn.Read(buf); err != nil {
+		return fmt.Errorf("can't read from conn: %w", err)
+	}
+
+	bb := bytes.NewBuffer(buf)
+	startLine, err := bb.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("can' read start line: %w", err)
+	}
+
+	split := strings.Split(startLine, " ")
+	if len(split) != 3 {
+		return errors.New("malformed start line")
+	}
+
+	status, stringStatus := 200, "OK"
+
+	if split[1] != "/" {
+		status, stringStatus = 404, "Not Found"
+	}
+
+	return respond(conn, status, stringStatus)
 }
 
 func main() {
@@ -35,8 +59,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := respond(conn, 200, "OK"); err != nil {
-		fmt.Println("Error responding: ", err.Error())
+	if err := checkStartLine(conn); err != nil {
+		fmt.Printf("can't check start line: %v", err)
 		os.Exit(1)
 	}
 }
